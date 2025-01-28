00ab5ee291b181fa5a52d2adecffe55934b57d55
Danilo <danilo.2693@gmail.com>
2025-01-28 18:23:05 -0500

    se agrega middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server'
 
-export function middleware(req) {
+
+export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   if (pathname.startsWith('/invitations')) {
