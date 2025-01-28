981f887b1027c39981ddacacf6ce296c10c0ea09
Danilo <danilo.2693@gmail.com>
2025-01-28 18:50:30 -0500

    update redirects config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    console.log('hola');
+    return [
+      {
+        source: '/((?!invitations).*)',
+        destination: '/invitations',
+        permanent: false
+      }
+    ];
+  }
 };
 
 export default nextConfig;
